41b8abd177f6864178ac30f295f0b4911f02527e
jonathanwxyz <mail4jonathanw@gmail.com>
2021-06-28 21:33:31 +0100

    Updated UI

--- src/bitcoinOptions.js
+++ src/bitcoinOptions.js
@@ -1,6 +1,6 @@
 export const bitcoinOptions = {
   BTC: { id: 0, conversion: 1, decimalPlaces: 8, symbol: "BTC" },
-  bit: { id: 1, conversion: 0.000001, decimalPlaces: 2, symbol: "bit" },
   sat: { id: 2, conversion: 0.00000001, decimalPlaces: 0, symbol: "sat" },
+  bit: { id: 1, conversion: 0.000001, decimalPlaces: 2, symbol: "bit" },
   mBTC: { id: 3, conversion: 0.001, decimalPlaces: 5, symbol: "mBTC" },
 };
--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Price = ({ price, symbol }) => {
   return (
     <div>
-      <h1>
+      <h1 className="font-weight-bold">
         1₿ = {symbol}
         {price}
       </h1>
